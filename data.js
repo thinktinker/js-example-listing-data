03c7de592641cbe74ff00be837b7cc0ace4e07e7
thinktinker <martinleongmo@gmail.com>
2026-04-22 16:50:49 +0800

    1st draft, creating a product listing

---
+++ data.js
@@ -0,0 +1,50 @@
+data = [
+    {
+      id: "1",
+      title: "Product 1 Title",
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem dolor atque incidunt voluptate non minima.",
+      imageUrl:
+        "https://images.unsplash.com/photo-1580999991243-9eee481d03fb?q=80&w=2126&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: "2",
+      title: "Product 2 Title",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum!",
+      imageUrl:
+        "https://images.unsplash.com/photo-1581327512036-94637d8cdc2a?q=80&w=2129&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: "3",
+      title: "Product 3 Title",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, animi?",
+      imageUrl:
+        "https://images.unsplash.com/photo-1460353581641-37baddab0fa2?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: "4",
+      title: "Product 4 Title",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem nesciunt quas dolorem.",
+      imageUrl:
+        "https://images.unsplash.com/photo-1704919266475-aa6302e25209?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: "5",
+      title: "Product 5 Title",
+      description:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt aliquam aliquid repudiandae natus inventore officiis soluta aut nemo.",
+      imageUrl:
+        "https://images.unsplash.com/photo-1681870539446-291dd1cb1e15?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: "6",
+      title: "Product 6 Title",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rem aliquam animi itaque obcaecati tenetur.",
+      imageUrl:
+        "https://images.unsplash.com/photo-1581088583450-8f6314b6feb1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  ];
